77cabebda9e7d986635e3c3fd4d61e60b2a13133
Miha <xd383727@gmail.com>
2021-11-13 07:25:00 +0300

    my

--- MopileApps/Server/MainServer.cs
+++ MopileApps/Server/MainServer.cs
@@ -88,24 +88,13 @@ namespace MopileApps.Client
 
 
                 HttpClient client = new HttpClient();
-<<<<<<< HEAD
-                //client.BaseAddress = new Uri(url);
-                while (true)
-                {
-                    string response = await client.GetStringAsync(url);
-                    var result = JToken.Parse(response);
-                    string rateInfo = result[0].ToString();
-                    //var rateInfo = JsonConvert.DeserializeObject<ViewNews>(result.ToString());
-                    Console.WriteLine(rateInfo);
-                    //this.id = rateInfo;
-=======
                 string response = await client.GetStringAsync(url);
                 var result = JToken.Parse(response);
                 string rateInfo = result.ToString();
 
                 for (int i = 0; i < result.ToString().Length; i++)
                 {
-             
+
                     this.News = result[i]["news"].ToString();
                     this.Autor = result[i]["autor"].ToString();
                     this.Date = result[i]["date"].ToString();
@@ -114,14 +103,6 @@ namespace MopileApps.Client
 
                 }
 
->>>>>>> a1c60c967999679a9ecdd9b06acf0049f04ee092
-
-                    this.News = result[0]["news"].ToString();
-                    this.News = result[1]["news"].ToString();
-                    this.Autor = result[0]["autor"].ToString();
-                    this.Date = result[0]["date"].ToString();
-                    this.Text = result[0]["text"].ToString();
-                }
             }
             catch (Exception ex)
             { Console.WriteLine(ex); }
