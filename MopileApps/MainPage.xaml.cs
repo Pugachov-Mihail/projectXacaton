f5cdcf8574c7c8d6c43120c676f903d74f1a96ca
Miha <xd383727@gmail.com>
2021-11-16 22:26:10 +0300

    add

--- MopileApps/MainPage.xaml.cs
+++ MopileApps/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Xamarin.Forms;
 using MopileApps.Client;
+using Newtonsoft.Json.Linq;
 using System;
 
 
@@ -12,14 +13,22 @@ namespace MopileApps
         {
             InitializeComponent();
             //
-            MainServer viewModel = new MainServer()
-            {
-                
-            };
-            viewModel.LoadData();
-            this.BindingContext = viewModel;
 
             
+            Server server = new Server();
+            server.GetRequest("/api/news/");
+            JToken a = server.ServerParse(server.response);
+            
+            ViewNews viewNews = new ViewNews();
+            for(int i=0; i<a.ToString().Length; i++)
+            {
+                viewNews.Title = a[0]["title"].ToString();
+            }
+
+            this.BindingContext = viewNews;
+
+            Console.WriteLine("_______" + server.response);
+            Console.WriteLine("_______" + server.ServerParse(server.response));
         }
     }
 }
---
+++ MopileApps/Server/Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+public class Server
+{
+	private string rootUrl = "";
+    public string response = "";
+    public JToken result = "";
+
+	public Server()
+	{
+	}
+
+	public async void GetRequest (string api)
+    {
+        string url = "https://acf5-185-34-240-5.ngrok.io" + api;
+        try
+        {
+            HttpClient client = new HttpClient();
+            this.response = await client.GetStringAsync(url);
+        }
+        catch (Exception ex)
+        { Console.WriteLine(ex.Message); }
+    }
+
+    public void Post (string url)
+    {
+
+    }
+
+    public void PutRequest (string url)
+    {
+
+    }
+
+    public void Delete (string url)
+    {
+
+    }
+
+    public JToken ServerParse (string response)
+    {
+        try
+        {
+            this.result = JToken.Parse(response);
+            return this.result;
+        }
+        catch (Exception ex)
+        { 
+            Console.WriteLine(ex.Message);
+            return this.result;
+        }
+    }
+}
