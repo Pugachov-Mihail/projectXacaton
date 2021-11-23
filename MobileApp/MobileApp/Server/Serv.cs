532ecf8942ca492387c38ed19066fc58df52cb97
Miha <xd383727@gmail.com>
2021-11-24 00:05:52 +0300

    create server

---
+++ MobileApp/MobileApp/Server/Serv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MobileApp.Server
+{
+    class Serv
+    {
+        HttpClient client;
+
+        public Serv()
+        {
+
+        }
+    }
+}
